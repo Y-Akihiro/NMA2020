1d074816623545b6d2c511545ea4b56703ced837
aki <akihiro.yamaguchi@nyu.edu>
2020-09-26 18:52:40 -0400

    psychometric curve update
    behavior-based psychometric curve

--- Project_decision dinos/functions.py
+++ Project_decision dinos/functions.py
@@ -101,14 +101,196 @@ def get_rightward(dat, cont_diff):
         * cont_diff - contrast difference between left and right
     Return:
         * rightward - % of mice respond right for each contrast difference
+        * response of -1 corresponds to 'right' and 1 to 'left' choices
     """
     rightward = np.zeros(len(np.unique(cont_diff)))
     unique, counts = np.unique(cont_diff, return_counts=True) # check the contrast differences and the number of occurences
 
     for i, val in enumerate(np.unique(cont_diff)):
         resp = dat['response'][cont_diff==val]
-        rightward[i] = np.count_nonzero(resp==1) / counts[i]*100
+        rightward[i] = np.count_nonzero(resp<0) / counts[i]*100
         
     return rightward
 
+def func(x,x0, y0, a,b):
+    '''
+    Four-parameter logistic function for model fitting
+    '''
+    return y0 + a/(1+np.exp(-(x-x0)/b))
 
+def get_right_hist_1(dat, cont_diff):
+    """
+    Inputs: 
+        * dat - trial data 
+        * cont_diff - contrast difference between left and right
+    Return:
+        * reasy_l - % of mice respond right for each contrast difference, previous trial is easy left
+        * rdiff_l - % of mice respond right for each contrast difference, previous trial is difficult left
+        * reasy_r - % of mice respond right for each contrast difference, previous trial is easy right
+        * rdiff_r - % of mice respond right for each contrast difference, previous trial is difficult right
+    """
+    unique, counts = np.unique(cont_diff, return_counts=True) # check the contrast differences and the number of occurences
+
+    n_el = n_dl = n_er = n_dr = n_zero = 0
+    
+    # Define easy/difficult left/right (boolean array)
+    easy_l = (cont_diff==-1)    #+ (cont_diff==-0.75)
+    diff_l = (cont_diff==-0.25) #+ (cont_diff==-0.5)
+    diff_r = (cont_diff==0.25)  #+ (cont_diff==0.5)
+    easy_r = (cont_diff==1)     #+ (cont_diff==0.75)
+
+    reasy_l = np.zeros((easy_l.sum(),9,2)) # 59 x 9
+    rdiff_l = np.zeros((diff_l.sum(),9,2)) # 52 x 9
+    reasy_r = np.zeros((easy_r.sum(),9,2)) # 41 x 9
+    rdiff_r = np.zeros((diff_r.sum(),9,2)) # 62 x 9
+    rzero = np.zeros(((cont_diff==0).sum(),9,2)) # 126 x 9
+
+    # Check the number of trials for each difficulties 
+    n_trials = np.zeros(6)
+    n_trials[0] = rzero.shape[0] 
+    n_trials[1] = reasy_l.shape[0]
+    n_trials[2] = rdiff_l.shape[0]
+    n_trials[3] = rdiff_r.shape[0]
+    n_trials[4] = reasy_r.shape[0]
+    n_trials[5] = dat['spks'].shape[1]
+    
+    for i in range(len(dat['response'])-1):
+        hist = cont_diff[i]                                  # previous trial difficulty (size) & direction (sign)
+        idx_cont = np.where(unique==cont_diff[i+1])[0][0]   # current trial difficulty and direction label (9 unique labels)
+
+        if hist == -1: #in unique[0:2]: # easy left
+            reasy_l[n_el, idx_cont,0] = dat['response'][i+1]
+            reasy_l[0,idx_cont,1] +=1
+            n_el += 1
+        elif hist == -0.25: #in unique[2:4]: # difficult left
+            rdiff_l[n_dl, idx_cont,0] = dat['response'][i+1]
+            rdiff_l[0, idx_cont,1] += 1
+            n_dl += 1
+        elif hist == 0.25: #in unique[5:7]: # difficult right
+            rdiff_r[n_dr, idx_cont,0] = dat['response'][i+1]
+            rdiff_r[0, idx_cont,1] += 1
+            n_dr += 1
+        elif hist == 1.0: #in unique[7:9]: # easy right
+            reasy_r[n_er, idx_cont,0] = dat['response'][i+1]
+            reasy_r[0, idx_cont,1] += 1
+            n_er += 1
+        elif hist == 0:
+            rzero[n_zero, idx_cont,0] = dat['response'][i+1]
+            rzero[0, idx_cont,1] += 1
+            n_zero += 1
+        else:
+            hist += 1 # some action. (whatever is fine to pass)
+    #         print('Check: something is wrong')
+
+    # Use np.divide(a, b, out=np.zeros(a.shape), where=b!=0) to avoid 0 division error
+    r_easyr = np.divide(np.count_nonzero(reasy_r[:,:,0]==1,axis=0),
+                        reasy_r[0,:,1], 
+                        out=np.zeros(np.count_nonzero(reasy_r[:,:,0]==1,axis=0).shape), 
+                        where=(reasy_r[0,:,1]!=0)) * 100
+    r_easyl = np.divide(np.count_nonzero(reasy_l[:,:,0]==1,axis=0),
+                        reasy_l[0,:,1], 
+                        out=np.zeros(np.count_nonzero(reasy_l[:,:,0]==1,axis=0).shape), 
+                        where=(reasy_l[0,:,1]!=0)) * 100
+    r_diffr = np.divide(np.count_nonzero(rdiff_r[:,:,0]==1,axis=0),
+                        rdiff_r[0,:,1], 
+                        out=np.zeros(np.count_nonzero(rdiff_r[:,:,0]==1,axis=0).shape), 
+                        where=(rdiff_r[0,:,1]!=0)) * 100
+    r_diffl = np.divide(np.count_nonzero(rdiff_l[:,:,0]==1,axis=0),
+                        rdiff_l[0,:,1], 
+                        out=np.zeros(np.count_nonzero(rdiff_l[:,:,0]==1,axis=0).shape), 
+                        where=(rdiff_l[0,:,1]!=0)) * 100
+    r_zero = np.divide(np.count_nonzero(rzero[:,:,0]==1,axis=0),
+                       rzero[0,:,1], 
+                       out=np.zeros(np.count_nonzero(rzero[:,:,0]==1,axis=0).shape), 
+                       where=(rzero[0,:,1]!=0)) * 100
+        
+    return r_easyr, r_easyl, r_diffr, r_diffl, r_zero, n_trials
+
+
+def get_right_hist_2(dat, cont_diff):
+    """
+    Inputs: 
+        * dat - trial data 
+        * cont_diff - contrast difference between left and right
+    Return:
+        * reasy_l - % of mice respond right for each contrast difference, previous trial is easy left
+        * rdiff_l - % of mice respond right for each contrast difference, previous trial is difficult left
+        * reasy_r - % of mice respond right for each contrast difference, previous trial is easy right
+        * rdiff_r - % of mice respond right for each contrast difference, previous trial is difficult right
+    """
+    unique, counts = np.unique(cont_diff, return_counts=True) # check the contrast differences and the number of occurences
+
+    n_el = n_dl = n_er = n_dr = n_zero = 0
+    
+    # Define easy/difficult left/right (boolean array)
+    easy_l = (cont_diff==-1) + (cont_diff==-0.75)
+    diff_l = (cont_diff==-0.25) + (cont_diff==-0.5)
+    diff_r = (cont_diff==0.25) + (cont_diff==0.5)
+    easy_r = (cont_diff==1) + (cont_diff==0.75)
+
+    reasy_l = np.zeros((easy_l.sum(),9,2)) # 59 x 9
+    rdiff_l = np.zeros((diff_l.sum(),9,2)) # 52 x 9
+    reasy_r = np.zeros((easy_r.sum(),9,2)) # 41 x 9
+    rdiff_r = np.zeros((diff_r.sum(),9,2)) # 62 x 9
+    rzero = np.zeros(((cont_diff==0).sum(),9,2)) # 126 x 9
+
+    # Check the number of trials for each difficulties 
+    n_trials = np.zeros(6)
+    n_trials[0] = rzero.shape[0] 
+    n_trials[1] = reasy_l.shape[0]
+    n_trials[2] = rdiff_l.shape[0]
+    n_trials[3] = rdiff_r.shape[0]
+    n_trials[4] = reasy_r.shape[0]
+    n_trials[5] = dat['spks'].shape[1]
+    
+    for i in range(len(dat['response'])-1):
+        hist = cont_diff[i]                                  # previous trial difficulty (size) & direction (sign)
+        idx_cont = np.where(unique==cont_diff[i+1])[0][0]   # current trial difficulty and direction label (9 unique labels)
+
+        if hist in unique[0:2]: # easy left
+            reasy_l[n_el, idx_cont,0] = dat['response'][i+1]
+            reasy_l[0,idx_cont,1] +=1
+            n_el += 1
+        elif hist in unique[2:4]: # difficult left
+            rdiff_l[n_dl, idx_cont,0] = dat['response'][i+1]
+            rdiff_l[0, idx_cont,1] += 1
+            n_dl += 1
+        elif hist in unique[5:7]: # difficult right
+            rdiff_r[n_dr, idx_cont,0] = dat['response'][i+1]
+            rdiff_r[0, idx_cont,1] += 1
+            n_dr += 1
+        elif hist in unique[7:9]: # easy right
+            reasy_r[n_er, idx_cont,0] = dat['response'][i+1]
+            reasy_r[0, idx_cont,1] += 1
+            n_er += 1
+        elif hist == 0:
+            rzero[n_zero, idx_cont,0] = dat['response'][i+1]
+            rzero[0, idx_cont,1] += 1
+            n_zero += 1
+        else:
+            hist += 1 # some action. (whatever is fine to pass)
+    #         print('Check: something is wrong')
+
+    # Use np.divide(a, b, out=np.zeros(a.shape), where=b!=0) to avoid 0 division error
+    r_easyr = np.divide(np.count_nonzero(reasy_r[:,:,0]==1,axis=0),
+                        reasy_r[0,:,1], 
+                        out=np.zeros(np.count_nonzero(reasy_r[:,:,0]==1,axis=0).shape), 
+                        where=(reasy_r[0,:,1]!=0)) * 100
+    r_easyl = np.divide(np.count_nonzero(reasy_l[:,:,0]==1,axis=0),
+                        reasy_l[0,:,1], 
+                        out=np.zeros(np.count_nonzero(reasy_l[:,:,0]==1,axis=0).shape), 
+                        where=(reasy_l[0,:,1]!=0)) * 100
+    r_diffr = np.divide(np.count_nonzero(rdiff_r[:,:,0]==1,axis=0),
+                        rdiff_r[0,:,1], 
+                        out=np.zeros(np.count_nonzero(rdiff_r[:,:,0]==1,axis=0).shape), 
+                        where=(rdiff_r[0,:,1]!=0)) * 100
+    r_diffl = np.divide(np.count_nonzero(rdiff_l[:,:,0]==1,axis=0),
+                        rdiff_l[0,:,1], 
+                        out=np.zeros(np.count_nonzero(rdiff_l[:,:,0]==1,axis=0).shape), 
+                        where=(rdiff_l[0,:,1]!=0)) * 100
+    r_zero = np.divide(np.count_nonzero(rzero[:,:,0]==1,axis=0),
+                       rzero[0,:,1], 
+                       out=np.zeros(np.count_nonzero(rzero[:,:,0]==1,axis=0).shape), 
+                       where=(rzero[0,:,1]!=0)) * 100
+        
+    return r_easyr, r_easyl, r_diffr, r_diffl, r_zero, n_trials
